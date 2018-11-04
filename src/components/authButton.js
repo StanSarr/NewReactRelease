0e2ef1f3198ced0b5e6c98aadc68bbcb6a2a98ec
stan <stan.sarr@koober.com>
2018-11-04 17:14:26 +0100

    clean authButton

--- src/components/authButton.js
+++ src/components/authButton.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { NavItem, Button } from "reactstrap";
 import * as Context from "../context";
 
-export default () => (
-  <Context.Language.Consumer>
-    {({ locale, translation, switchLocale }) => (
-      <Context.Auth.Consumer>
-        {Auth => (
-          <NavItem right>
-            {Auth.isLoggedIn ? (
-              <Button onClick={() => Auth.logOut()}>
-                {translation.logout}
-              </Button>
-            ) : (
-              <Button onClick={() => Auth.logIn()}>{translation.login}</Button>
-            )}
-          </NavItem>
-        )}
-      </Context.Auth.Consumer>
-    )}
-  </Context.Language.Consumer>
-);
+// const someContext = React.createContext({ some: "A" });
+export default () => {
+  const [count, setCount] = useState(0);
+  console.log(count);
+  return (
+    <Context.Language.Consumer>
+      {({ locale, translation, switchLocale }) => (
+        <Context.Auth.Consumer>
+          {Auth => (
+            <NavItem right={"true"}>
+              {Auth.isLoggedIn ? (
+                <Button onClick={() => Auth.logOut()}>
+                  {translation.logout}
+                </Button>
+              ) : (
+                <Button onClick={() => Auth.logIn()}>
+                  {translation.login}
+                </Button>
+              )}
+            </NavItem>
+          )}
+        </Context.Auth.Consumer>
+      )}
+    </Context.Language.Consumer>
+  );
+};
